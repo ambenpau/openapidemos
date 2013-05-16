fd42ea7ac0155c62ec72437472683759e657938f
macoscar <oscar.iglesias.lopez@icloud.com>
2013-05-16 09:53:28 +0200

    Initial Commit

---
+++ openApi/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  openApi
+//
+//  Created by macoscar on 16/05/13.
+//  Copyright (c) 2013 Universitat Oberta de Catalunya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ openApi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  openApi
+//
+//  Created by macoscar on 16/05/13.
+//  Copyright (c) 2013 Universitat Oberta de Catalunya. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
