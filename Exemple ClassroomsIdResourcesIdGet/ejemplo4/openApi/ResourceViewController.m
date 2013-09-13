2f44306f251fddd3f2a5eff5ccfe07c93f37cada
ambenpau <pauinho_FCB@hotmail.com>
2013-09-13 11:15:48 +0200

    Funcions openApi
    Exemples d'us de les funcions de la openApi per IOS

---
+++ Exemple ClassroomsIdResourcesIdGet/ejemplo4/openApi/ResourceViewController.m
@@ -0,0 +1,68 @@
+//
+//  ResourceViewController.m
+//  ejemplo4
+//
+//  Created by UOC on 16/07/13.
+//  Copyright (c) 2013 UOC. All rights reserved.
+//
+
+#import "ResourceViewController.h"
+
+@interface ResourceViewController ()
+
+@end
+
+@implementation ResourceViewController
+
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    self.recurs = [[Resource alloc]init];
+    [self carregarRecurs];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+-(void) carregarRecurs
+{
+    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+    // Definim la URL per a obtenir les dades del recurs amb la id indicada.
+    NSURL *resourcesURL = [NSURL URLWithString:[NSString stringWithFormat:@"http://denver.uoc.es:8080/webapps/uocapi/api/v1/classrooms/%@/resources/%@?access_token=%@", self.aula.identifier, self.idRecurs, self.auth.accessToken]];
+    
+    dispatch_queue_t backgroundQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0);
+    
+    dispatch_async(backgroundQueue, ^{
+        NSData *resourcesData = [NSData dataWithContentsOfURL:resourcesURL ];
+        NSLog(@"Recurs amb id : %@ - %@" , self.idRecurs , [[NSString alloc]initWithData:resourcesData encoding:NSUTF8StringEncoding]);
+        NSDictionary *resourcesDict = [NSJSONSerialization JSONObjectWithData:resourcesData options:0 error:nil];
+        
+        if ([resourcesDict valueForKey:@"error"]) {
+            NSLog(@"%@: %@", [resourcesDict valueForKey:@"error"], [resourcesDict valueForKey:@"error_description"]);
+            return;
+        }
+        
+        [self.recurs setDatos:resourcesDict];
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // La vista nomes es pot actualitzar en la cua principal.
+            [self mostrarRecurs];
+            [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+        });
+    });
+}
+
+- (void)mostrarRecurs
+{
+    self.identifier.text    = [[NSString alloc] initWithFormat:@"id recurs: %@", self.recurs.identifier];
+    self.type.text    = [[NSString alloc] initWithFormat:@"tipus recurs: %@", self.recurs.type];
+    self.subtype.text    = [[NSString alloc] initWithFormat:@"subtipus recurs: %@", self.recurs.subtype];
+    self.titol.text    = [[NSString alloc] initWithFormat:@"títol recurs: %@" , self.recurs.title];
+    self.code.text    = [[NSString alloc] initWithFormat:@"codi recurs: %@", self.recurs.code];
+    self.domainId.text    = [[NSString alloc] initWithFormat:@"id aula: %@", self.recurs.domainId];
+}
+@end
